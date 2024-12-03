ea4d0eb2bac9d4c90def969661ee86c8cdedb64d
GlipAmongus <pawlunumber2@gmail.com>
2024-12-03 20:48:18 +0100

    state, find plan file skeletons

---
+++ FindPlan.java
@@ -0,0 +1,8 @@
+public class FindPlan {
+    public int[] initialStete1 = { 8, 6, 7, 2, 5, 4, 3, 0, 1 }; // Test initial state 1
+    public int[] initialState2 = { 6, 4, 7, 8, 5, 0, 3, 2, 1 }; // Test initial state 2
+    public int[] goalState = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+
+    State[] plan;
+
+}
---
+++ State.java
@@ -0,0 +1,29 @@
+public class State {
+  public int emptyTileIndex; // Array index of zero (empty tile) (i.e. {1,2,3,4,5,6,7,8,0} eTI = 8)
+
+  public boolean redundantState;
+
+  public int gCost; // Cost from start state to state n
+  public int hCost; // Cost from state n to goal state (when using hueristic)
+  public int misplacedTiles; // Count of number of displaced tiles
+
+  // Will redundancy be checked by retracing and comparing states to parents, or
+  // keeping a dynamic array during run time?
+  public State parent; // Store state's parent state
+  public State[] children; // Current state's reachable children
+
+  public State[] Children() {
+
+    return children;
+  }
+
+  public int fCost() {
+    return hCost + gCost;
+  }
+
+  public void redundantState(State current, State parent) {
+    if (current == current.parent) {
+      this.redundantState = true;
+    }
+  }
+}
