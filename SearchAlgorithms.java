091d1fc3d7a42a2579370410aa33f35c010400e9
GlipAmongus <pawlunumber2@gmail.com>
2024-12-07 01:17:07 +0100

    fixed ehc, alls working, todo - heap structure

--- SearchAlgorithms.java
+++ SearchAlgorithms.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.function.BiFunction;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -61,7 +61,7 @@ public class SearchAlgorithms {
             for (State child : headState.children()) {
                 if (closedStates.contains(child) || inEdgeStates.contains(child))
                     continue;
-                
+
                 child.hCost = DistanceFunction.apply(child, goalState);
                 edgeStates.add(child);
                 inEdgeStates.add(child);
@@ -111,7 +111,7 @@ public class SearchAlgorithms {
                     child.gCost = CostToChild;
                     child.hCost = DistanceFunction.apply(child, goalState);
 
-                    if (!inEdgeStates.contains(child)){
+                    if (!inEdgeStates.contains(child)) {
                         edgeStates.add(child);
                         inEdgeStates.add(child);
                     }
@@ -125,41 +125,40 @@ public class SearchAlgorithms {
     public static State FindEnforcedPlan(State bestState, BiFunction<State, State, Integer> DistanceFunction) {
         State goalState = new State(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 0 }, null);
 
-        ArrayList<State> edgeStates = new ArrayList<>();
-        ArrayList<State> children = new ArrayList<>();
-
+        Queue<State> edgeStates = new LinkedList<>();
         HashSet<State> closedStates = new HashSet<>();
-        // HashSet<State> inEdgeStates = new HashSet<>();
+        HashSet<State> inEdgeStates = new HashSet<>();
 
         edgeStates.add(bestState);
-        // inEdgeStates.add(initialState);
+        bestState.hCost = DistanceFunction.apply(bestState, goalState);
 
         while (!edgeStates.isEmpty()) { // traverse all edge States
-            State currenState = edgeStates.get(0);
+            State currenState = edgeStates.poll();
+            inEdgeStates.remove(currenState);
+            closedStates.add(currenState);
 
-            for (State child : currenState.children()) {
-                if (closedStates.contains(child))
-                    continue;
-                else 
-                    children.add(child);
+            if (Arrays.equals(currenState.puzzleStructure, goalState.puzzleStructure)) {
+                int unique = closedStates.size() + edgeStates.size();
+                System.out.println("unique: " + unique);
+                return currenState;
             }
-            while (!children.isEmpty()) { // traverse all edge States
-                State nextState = children.get(0);
 
-                if (Arrays.equals(nextState.puzzleStructure, goalState.puzzleStructure)) {
-                    int unique = closedStates.size() + edgeStates.size();
-                    System.out.println("unique: " + unique);
-                    return nextState;
-                }
+            for (State child : currenState.children()) { // traverse all edge States
+                if (closedStates.contains(child) && inEdgeStates.contains(child))
+                    continue;                
 
-                if (nextState.hCost > bestState.hCost) {
+                child.hCost = DistanceFunction.apply(child, goalState);
+                if (child.hCost < bestState.hCost) {
+                    bestState = child;
                     edgeStates.clear();
-                    children.clear();
-                    bestState = nextState;    
+                    edgeStates.add(child);
+                    inEdgeStates.add(child);
+                    break;
+                } else {
+                    edgeStates.add(child);
+                    inEdgeStates.add(child);
                 }
-
-                edgeStates.add(nextState);
-            }  
+            }
         }
         return bestState;
     }
